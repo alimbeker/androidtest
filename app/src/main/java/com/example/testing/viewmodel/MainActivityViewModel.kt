1e911057b7bfb1a24cdeebacb6180ae84569f9c1
Alimbekerov Amir <200103054@stu.sdu.edu.kz>
2023-07-27 15:55:16 +0600

    add backing property in viewmodel

--- app/src/main/java/com/example/testing/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/testing/viewmodel/MainActivityViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.testing.viewmodel
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainActivityViewModel: ViewModel() {
+    private val _word = MutableStateFlow("")
+    val word: StateFlow<String> get() = _word
 
+    // Function to update the "Hello" text state
+    fun setHelloText(text: String) {
+        _word.value = text
+    }
 }
